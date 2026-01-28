98bd67d3b98065608622053f3e6fa6c4b1cadacc
ConnerWill <conner.will@outlook.com>
2026-01-28 15:12:42 -0600

    Updated completion files

--- completion/yayfzf_completion.sh
+++ completion/yayfzf_completion.sh
@@ -10,26 +10,26 @@ _yayfzf_complete() {
 
     # Complete --long-options
     if [[ ${cur} == --* ]]; then
-        COMPREPLY=( $(compgen -W "--help --keybindings --init-config --show-config --version" -- "$cur") )
+        COMPREPLY=( $(compgen -W "--help --keybindings --init-config --show-config --verbose --version" -- "${cur}") )
         return
     fi
 
     # Complete -short options
     if [[ ${cur} == -* ]]; then
-        COMPREPLY=( $(compgen -W "-h -k -V" -- "$cur") )
+        COMPREPLY=( $(compgen -W "-h -k -V" -- "${cur}") )
         return
     fi
 
     # If we're at position 1 (first word after command) → show options only
     if (( cword == 1 )); then
-        local opts="-h --help -k --keybindings --init-config --show-config -V --version"
-        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+        local opts="-h --help -k --keybindings -i --init-config -s --show-config -v --verbose -V --version"
+        COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
         return 0
     fi
 
     # After any recognized option → nothing more
     case "${prev}" in
-        -h|--help|-k|--keybindings|--init-config|--show-config|-V|--version)
+        -h|--help|-k|--keybindings|-i|--init-config|-s|--show-config|-v|--verbose|-V|--version)
             return 0
             ;;
     esac
