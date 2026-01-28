d3b43373cdcb9bc18fdbe701214e79a15f7a5a87
ConnerWill <conner.will@outlook.com>
2026-01-28 15:50:51 -0600

    Added --help-man option to script and completion

--- completion/yayfzf_completion.sh
+++ completion/yayfzf_completion.sh
@@ -10,7 +10,7 @@ _yayfzf_complete() {
 
     # Complete --long-options
     if [[ ${cur} == --* ]]; then
-        COMPREPLY=( $(compgen -W "--help --keybindings --init-config --show-config --verbose --version" -- "${cur}") )
+        COMPREPLY=( $(compgen -W "--help --help-man --keybindings --init-config --show-config --verbose --version" -- "${cur}") )
         return
     fi
 
@@ -22,14 +22,14 @@ _yayfzf_complete() {
 
     # If we're at position 1 (first word after command) → show options only
     if (( cword == 1 )); then
-        local opts="-h --help -k --keybindings -i --init-config -s --show-config -v --verbose -V --version"
+        local opts="-h --help --help-man -k --keybindings -i --init-config -s --show-config -v --verbose -V --version"
         COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
         return 0
     fi
 
     # After any recognized option → nothing more
     case "${prev}" in
-        -h|--help|-k|--keybindings|-i|--init-config|-s|--show-config|-v|--verbose|-V|--version)
+        -h|--help|--help-man|-k|--keybindings|-i|--init-config|-s|--show-config|-v|--verbose|-V|--version)
             return 0
             ;;
     esac
